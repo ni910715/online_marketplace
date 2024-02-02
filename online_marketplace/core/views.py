14207b961b6e7dbfb1d69bf90285d257a24c57d1
ni910715 <bigpjg0616@gmail.com>
2024-02-02 17:26:50 +0800

    finished front page

--- online_marketplace/core/views.py
+++ online_marketplace/core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from item.models import Category, Item
 
 # Create your views here.
 
 def index(response):
-    return render(response, 'core/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(response, 'core/index.html', {'items':items, 'categories':categories})
 
 def contact(response):
     return render(response, 'core/contact.html')
