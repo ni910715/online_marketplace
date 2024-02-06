2ce5f7355c7c1a53d2b01d6c2ac8d1ced08525f3
ni910715 <bigpjg0616@gmail.com>
2024-02-06 16:17:40 +0800

    description and related items

--- online_marketplace/item/views.py
+++ online_marketplace/item/views.py
@@ -4,4 +4,5 @@ from .models import Item
 # Create your views here.
 def detail(response, pk):
     item = get_object_or_404(Item, pk=pk)
-    return render(response, 'item/detail.html', {'item':item})
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+    return render(response, 'item/detail.html', {'item':item, 'related_items':related_items})
