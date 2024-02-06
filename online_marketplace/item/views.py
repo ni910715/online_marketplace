fb072dce13de99e79e1ef0ff42f5e089177d711e
ni910715 <bigpjg0616@gmail.com>
2024-02-06 12:46:16 +0800

    add item detail page

--- online_marketplace/item/views.py
+++ online_marketplace/item/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Item
 
 # Create your views here.
+def detail(response, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(response, 'item/detail.html', {'item':item})
