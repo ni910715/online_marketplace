4614b286793b28ad9ce4056cf62fbdeb3e56617c
ni910715 <bigpjg0616@gmail.com>
2024-02-16 15:06:13 +0800

    custom signup form

--- online_marketplace/core/views.py
+++ online_marketplace/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
 from .forms import SignupForm
 
@@ -12,7 +12,15 @@ def index(response):
 def contact(response):
     return render(response, 'core/contact.html')
 
-def signup(request):
-    form = SignupForm()
+def signup(response):
+    if response.method == 'POST':
+        form = SignupForm(response.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignupForm()
     
-    return render(request, 'core/signup.html', {'form':form})
+    return render(response, 'core/signup.html', {'form':form})
